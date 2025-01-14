c05d731ba52563fd97d3d37b7bd7d6d9159bceab
RuanPablo-7 <ToyaMON777@outlook.com>
2025-01-14 15:55:51 -0300

    adicionei a tualização em tempo real e a converção dos valores

--- script.js
+++ script.js
@@ -4,72 +4,72 @@ let valueMoney = document.getElementById('value-money')
 let valueMoneyConverted = document.getElementById('value-money-converted')
 let inputNumber1 = document.getElementById('input-number-1')
 let inputNumber2 = document.getElementById('input-number-2')
-console.log(selectMoney)
+let theLastUpdate = document.getElementById('dateof-update-money')
 
-inputNumber1.addEventListener('change', () => {
-   
-    console.log(inputNumber1.value)
-    inputNumber1.textContent = 3
-})
 
-const api = fetch("https://v6.exchangerate-api.com/v6/23a3ebe869b290c1ed195f22/latest/USD", {
+
+const api = fetch("https://v6.exchangerate-api.com/v6/23a3ebe869b290c1ed195f22/latest/EUR/", {
     method: "GET",
     headers: {
         "Content-Type": "application/json",
     }
-
 })
 
-.then(response => response.json())
-.then(data => {
+    .then(response => response.json())
+    .then(data => {
 
-    console.log(data);
-    const rates = data.conversion_rates
+        console.log(data);
+        theLastUpdate.textContent = data.time_last_update_utc
+        const rates = data.conversion_rates
 
-    Object.entries(rates).forEach(([currency, rate],index) => {
-       
+        Object.entries(rates).forEach(([currency, rate], index) => {
+            let option = document.createElement('option')
+            option.innerHTML = `${currency}: ${rate}`
 
-        let option = document.createElement('option')
-        option.innerHTML = `${currency}: ${rate}`
+            let option2 = document.createElement('option')
+            option2.innerHTML = `${currency}: ${rate}`
 
-        let option2 = document.createElement('option')
-        option2.innerHTML = `${currency}: ${rate}`
+            selectMoney.appendChild(option)
+            selectMoney2.appendChild(option2)
+        })
+        valueMoney.textContent = selectMoney.value
+        valueMoneyConverted.textContent = selectMoney2.value
 
-        selectMoney.appendChild(option)
-        selectMoney2.appendChild(option2)
-        
-        
-        
-    })
-    
-    document.addEventListener('click', () => {
-        defineSelect1(selectMoney.value, selectMoney2.value)
-
- })
-
-})
+        document.addEventListener('click', () => {
+            defineSelect1(selectMoney.value, selectMoney2.value)
+        })
+    }) .catch(error => console.log(error('Erro', error)))
 
 
-function defineSelect1 (value1, value2) {
-  valueMoney.textContent = value1
-  valueMoneyConverted.textContent = value2
 
-  console.log(value1)
-  let palavras = value1.split(" ")
-  let palavras1 = value2.split(" ")
-  console.log(palavras[1])
-  console.log(palavras1[1])
-  convertMoney(palavras[1], palavras1[1])
- 
-  
+function defineSelect1(value1, value2) {
+    valueMoney.textContent = value1
+    valueMoneyConverted.textContent = value2
+
+    console.log(value1)
+    let palavras = value1.split(" ")
+    let palavras1 = value2.split(" ")
+    console.log(palavras[1])
+    console.log(palavras1[1])
+    convertMoney(palavras[1], palavras1[1])
 }
-
 
 function convertMoney(value1, value2) {
-    
-    //let mult = parseFloat(value1) * parseFloat(value2)
-    console.log(mult)
+
+    inputNumber1.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+            let mult = parseFloat(inputNumber1.value) * parseFloat(value2)
+            inputNumber2.value = mult.toFixed(2)
+            console.log(mult)
+
+        }
+    })
+
+    inputNumber2.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter') {
+            let mult = parseFloat(inputNumber2.value) / parseFloat(value2)
+            console.log(mult.toFixed(2))
+            inputNumber1.value = mult.toFixed(2)
+        }
+    })
 }
-
-
-console.log()
